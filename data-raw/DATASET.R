92a994792ac6c4bfc8ec35e86e02515cb0f1e1e0
Youngjun Na <ruminoreticulum@gmail.com>
2021-12-08 12:58:16 +0900

    update README and add hanwoo_farm

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,3 @@
 ## code to prepare `DATASET` dataset goes here
 
-hanwoo_kpn <- readxl::read_excel("/Users/youngjunna/Github/adatalab/hanwoo/data-raw/kpn-nias-2108.xlsx")
-hanwoo_kpn <- janitor::clean_names(hanwoo_kpn, case = "lower_camel", ascii = FALSE)
-
-usethis::use_data(hanwoo_kpn, overwrite = TRUE)
+usethis::use_data(DATASET, overwrite = TRUE)
