9ad2ac0120f1b2a43073a57e60c657e9bc7fb165
Youngjun Na <ruminoreticulum@gmail.com>
2022-02-03 14:48:05 +0900

    update kpn

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,16 @@
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
+
+library(dplyr)
+
+names(hanwoo::hanwoo_kpn)
+
+hanwoo_kpn <- readxl::read_excel("kpn-nias-2202.xlsx") %>%
+  janitor::clean_names(case = "lower_camel", ascii = FALSE)
+
+names(hanwoo_kpn)
+
+usethis::use_data(hanwoo_kpn, overwrite = TRUE)
+
+# save(hanwoo_kpn, file = "hanwoo_kpn.rda")
