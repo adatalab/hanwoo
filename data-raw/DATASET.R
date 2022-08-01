2def738f5d9e4708a3d33d0de16c3c18040ca661
Youngjun Na <ruminoreticulum@gmail.com>
2022-08-01 16:56:28 +0900

    add new kpn 2208

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,12 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# usethis::use_data(DATASET, overwrite = TRUE)
 
 library(dplyr)
 
 names(hanwoo::hanwoo_kpn)
 
-hanwoo_kpn <- readxl::read_excel("kpn-nias-2202.xlsx") %>%
+hanwoo_kpn <- readxl::read_excel("kpn-nias-2208.xlsx") %>%
   janitor::clean_names(case = "lower_camel", ascii = FALSE)
 
 names(hanwoo_kpn)
